7eabd232ac7984dda5960f0a6bbef56cf99724ea
Mehdi_Tech <109386095+MehdiTechDev@users.noreply.github.com>
2023-03-16 21:08:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typed", {
+    strings: ["Under development.", 
+    "Can be finished in 7 days so come back ;)"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true
+});
