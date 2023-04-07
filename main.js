06bf7fcf337693950148f80c93822b5eddc8e4ad
Mehdi_Tech <109386095+MehdiTechDev@users.noreply.github.com>
2023-04-07 18:30:42 +0000

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,4 @@ var typed = new Typed("#typed", {
     backSpeed: 70,
     loop: true
 });
+console.log("I'm working on this portfolio. It will not be finished in 7 days. But come back every week ;)\n\t\t\t\t - Mehdi");
