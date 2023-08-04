3862299efaa13c0ce259a4c54e570b3ea0ba0c67
Mehdi_Tech <109386095+MehdiTechDev@users.noreply.github.com>
2023-08-04 18:38:20 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed("#typed", {
-    strings: ["Under development.", 
-    "Can be finished in 7 days so come back ;)"],
-    typeSpeed: 100,
-    backSpeed: 70,
-    loop: true
-});
-console.log("I'm working on this portfolio. It will not be finished in 7 days. But come back every week ;)\n\t\t\t\t - Mehdi");
